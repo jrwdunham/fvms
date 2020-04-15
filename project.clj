3bb20470998316e6e517affa60138cf38f3d32b1
Joel Dunham <jrwdunham@gmail.com>
2020-04-15 09:46:35 -0700

    Handle POST lead request from VB-Tripleseat
    - Add handling of POST /v1/leads/create.js requests from vb-tripleseat
    - Add logging and timbre deps

--- project.clj
+++ project.clj
@@ -1,8 +1,10 @@
 (defproject fake-venue-management-service "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.9.0"]
+                 [org.clojure/tools.logging "0.5.0"]
                  [ring/ring-core "1.6.3"]
                  [ring/ring-jetty-adapter "1.6.3"]
                  [ring/ring-devel "1.6.3"]
+                 [com.taoensso/timbre "4.10.0"]
                  [cheshire "5.8.1"]
                  [clojure.java-time "0.3.2"]]
   :ring {:handler fvms.core/app
--- src/fvms/core.clj
+++ src/fvms/core.clj
@@ -2,10 +2,17 @@
   "Fake Venue Management web service. Emulates a venue management web service
   that can process requests related to the creation of leads on venues/spaces.
   Constructs a simple in-memory state."
-  (:require [clojure.string :as str]
+  (:require [clojure.pprint :as pprint]
+            [clojure.string :as str]
+            [clojure.tools.logging :as log]
+            [clojure.walk :as w]
             [cheshire.core :as ch]))
 
 (def inquiries (atom {}))
+
+(defn pp-str
+  [x]
+  (with-out-str (pprint/pprint x)))
 
 (def json-header {"Content-Type" "application/json"})
 
@@ -66,8 +73,13 @@
   (let [inquiry-id (get-resource-id uri inquiries-prefix)
         inquiry (get @inquiries inquiry-id
                      {:error true
-                      :message (format "There is no inquiry with id %s."
-                                       inquiry-id)})]
+                      :message
+                      (format "There is no inquiry with id %s. The known IDs are:\n%s\n"
+                              inquiry-id
+                              (->> @inquiries
+                                   keys
+                                   (map str)
+                                   (str/join "\n")))})]
     {:status (if (:error inquiry) 404 200)
      :body inquiry}))
 
@@ -85,20 +97,36 @@
     (swap! inquiries assoc id (assoc body :inquiry-id id))
     {:status 201 :body id}))
 
-(defn json-encode [handler]
+(defmethod router :v1-leads-create.js
+  [{:keys [body] :as r}]
+  (let [id (format "fake_tripleseat_lead_id_%s"
+                   (-> body w/keywordize-keys :vb_lead_id))]
+    (swap! inquiries assoc id (assoc body :lead-id id))
+    {:status 201 :body id}))
+
+(defn json-encode
+  [handler]
   (fn [request]
     (let [response (handler request)]
       (merge response
              {:headers json-header
               :body (ch/generate-string (:body response))}))))
 
-(defn json-decode [handler]
+(defn json-decode
+  [handler]
   (fn [request]
     (let [body (-> request
                    :body
                    (slurp :encoding "UTF-8")
                    ch/parse-string)]
       (handler (assoc request :body body)))))
+
+(defn debug
+  [handler]
+  (fn [{:keys [uri request-method] :as request}]
+    (log/infof "%s request to %s" request-method uri)
+    (log/infof (pp-str @inquiries))
+    (handler request)))
 
 (def app (-> router
              (json-encode)
